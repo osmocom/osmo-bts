b9880bc81289fa13b12801abcfc77fe6e83b8a94
Andreas Eversberg <jolly@eversberg.eu>
2013-02-14 12:22:42 +0100

    TRX: Allow transcoding of TCH FR with MSB first (RTP) or LSB first (E1)

--- src/osmo-bts-trx/tch_fr.h
+++ src/osmo-bts-trx/tch_fr.h
@@ -1,7 +1,8 @@
 #ifndef _TCH_FR_H
 #define _TCH_FR_H
 
-int tch_fr_decode(uint8_t *tch_data, sbit_t *bursts);
-int tch_fr_encode(ubit_t *bursts, uint8_t *tch_data, int len);
+int tch_fr_decode(uint8_t *tch_data, sbit_t *bursts, int network_order);
+int tch_fr_encode(ubit_t *bursts, uint8_t *tch_data, int len,
+	int network_order);
 
 #endif /* _TCH_FR_H */
