a5608d092db095f1043f7e471a938feb8b45f09d
Harald Welte <laforge@gnumonks.org>
2013-03-07 18:38:39 +0000

    measurement: use new 'struct gsm_meas_rep_unidir' of libosmocore

--- src/common/measurement.c
+++ src/common/measurement.c
@@ -178,10 +178,10 @@ int lchan_meas_check_compute(struct gsm_lchan *lchan, uint32_t fn)
 		irssi_sub_sum);
 
 	/* store results */
-	lchan->meas.res.rxlev_full = dbm2rxlev((int)irssi_full_sum * -1);
-	lchan->meas.res.rxlev_sub = dbm2rxlev((int)irssi_sub_sum * -1);
-	lchan->meas.res.rxqual_full = ber10k_to_rxqual(ber_full_sum);
-	lchan->meas.res.rxqual_sub = ber10k_to_rxqual(ber_sub_sum);
+	lchan->meas.ul_res.full.rx_lev = dbm2rxlev((int)irssi_full_sum * -1);
+	lchan->meas.ul_res.sub.rx_lev = dbm2rxlev((int)irssi_sub_sum * -1);
+	lchan->meas.ul_res.full.rx_qual = ber10k_to_rxqual(ber_full_sum);
+	lchan->meas.ul_res.sub.rx_qual = ber10k_to_rxqual(ber_sub_sum);
 
 	lchan->meas.flags |= LC_UL_M_F_RES_VALID;
 	lchan->meas.num_ul_meas = 0;
@@ -194,10 +194,10 @@ int lchan_meas_check_compute(struct gsm_lchan *lchan, uint32_t fn)
 /* build the 3 byte RSL uplinke measurement IE content */
 int lchan_build_rsl_ul_meas(struct gsm_lchan *lchan, uint8_t *buf)
 {
-	buf[0] = (lchan->meas.res.rxlev_full & 0x3f); /* FIXME: DTXu support */
-	buf[1] = (lchan->meas.res.rxlev_sub & 0x3f);
-	buf[2] = ((lchan->meas.res.rxqual_full & 7) << 3) |
-					(lchan->meas.res.rxqual_sub & 7);
+	buf[0] = (lchan->meas.ul_res.full.rx_lev & 0x3f); /* FIXME: DTXu support */
+	buf[1] = (lchan->meas.ul_res.sub.rx_lev & 0x3f);
+	buf[2] = ((lchan->meas.ul_res.full.rx_qual & 7) << 3) |
+					(lchan->meas.ul_res.sub.rx_qual & 7);
 
 	return 3;
 }
