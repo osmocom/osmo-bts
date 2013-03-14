7ece637e5b9547dbddd73e871d05b6d305ea1267
Andreas Eversberg <jolly@eversberg.eu>
2013-03-14 07:56:05 +0100

    TRX: Code cleanup, prepare for other codecs than GSM full rate

--- src/osmo-bts-trx/gsm0503_mapping.h
+++ src/osmo-bts-trx/gsm0503_mapping.h
@@ -4,7 +4,7 @@
 void gsm0503_xcch_burst_unmap(sbit_t *iB, sbit_t *eB, sbit_t *hl, sbit_t *hn);
 void gsm0503_xcch_burst_map(ubit_t *iB, ubit_t *eB, const ubit_t *hl,
 	const ubit_t *hn);
-void gsm0503_tch_fr_burst_unmap(sbit_t *iB, sbit_t *eB, sbit_t *h, int odd);
-void gsm0503_tch_fr_burst_map(ubit_t *iB, ubit_t *eB, const ubit_t *h, int odd);
+void gsm0503_tch_burst_unmap(sbit_t *iB, sbit_t *eB, sbit_t *h, int odd);
+void gsm0503_tch_burst_map(ubit_t *iB, ubit_t *eB, const ubit_t *h, int odd);
 
 #endif /* _0503_INTERLEAVING_H */
