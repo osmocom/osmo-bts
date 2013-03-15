e45fc8635926f70b0e19d5214f1f8b8fd06068aa
Holger Hans Peter Freyther <zecke@selfish.org>
2013-03-16 00:24:24 +0100

    respawn: Adjust the oom score for the supervisor and bts/pcu, increase sleep
    Make the script mostly unkillable due to OOM and make sure that the
    process has a score of zero. Wait 10 seconds before re-launching.
    
    The combination of ( && exec ) & appears to save one sub-process. The
    script has been tested with bash and busybox's ash.

--- contrib/respawn-only.sh
+++ contrib/respawn-only.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+PID=$$
+echo "-1000" > /proc/$PID/oom_score_adj
+
+
 while [ -f $1 ]; do
-	nice -n -20 $*
+	(echo "0" > /proc/self/oom_score_adj && exec nice -n -20 $*) &
+	LAST_PID=$!
+	wait $LAST_PID
+	sleep 10s
 done
