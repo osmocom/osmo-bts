6a32f0499fd5e5d5779abac8902017461cf2800a
Andreas Eversberg <jolly@eversberg.eu>
2013-03-17 16:33:11 +0100

    Get RSSI from received uplink data and send to PCU
    This bumps the PCU API version and thus requires a new version of the
    code on the sysmoBTS side!

--- include/osmo-bts/pcu_if.h
+++ include/osmo-bts/pcu_if.h
@@ -5,7 +5,8 @@ int pcu_tx_info_ind(void);
 int pcu_tx_rts_req(struct gsm_bts_trx_ts *ts, uint8_t is_ptcch, uint32_t fn,
 	uint16_t arfcn, uint8_t block_nr);
 int pcu_tx_data_ind(struct gsm_bts_trx_ts *ts, uint8_t is_ptcch, uint32_t fn,
-	uint16_t arfcn, uint8_t block_nr, uint8_t *data, uint8_t len);
+	uint16_t arfcn, uint8_t block_nr, uint8_t *data, uint8_t len,
+	int8_t rssi);
 int pcu_tx_rach_ind(struct gsm_bts *bts, int16_t qta, uint8_t ra, uint32_t fn);
 int pcu_tx_time_ind(uint32_t fn);
 int pcu_tx_pag_req(const uint8_t *identity_lv, uint8_t chan_needed);
