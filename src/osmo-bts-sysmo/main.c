c741cce7d8c1a64b6b46dcd9db5e634e6fbbbc54
Andreas Eversberg <jolly@eversberg.eu>
2014-01-13 14:46:01 +0100

    Fix: Call e1inp_vty_init() before reading config file

--- src/osmo-bts-sysmo/main.c
+++ src/osmo-bts-sysmo/main.c
@@ -319,6 +319,7 @@ int main(int argc, char **argv)
 
 	bts = gsm_bts_alloc(tall_bts_ctx);
 	vty_init(&bts_vty_info);
+	e1inp_vty_init();
 	bts_vty_init(bts, &bts_log_info);
 
 	handle_options(argc, argv);
