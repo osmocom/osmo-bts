adddb65f4656b0b03b24fa3868e2a8c555af18da
Holger Hans Peter Freyther <holger@moiji-mobile.com>
2014-08-25 08:14:55 +0200

    sysmobts: Remove left-over from non-generic power control

--- src/osmo-bts-sysmo/utils.h
+++ src/osmo-bts-sysmo/utils.h
@@ -11,9 +11,4 @@ int band_femto2osmo(GsmL1_FreqBand_t band);
 int sysmobts_select_femto_band(struct gsm_bts_trx *trx, uint16_t arfcn);
 
 int sysmobts_get_nominal_power(struct gsm_bts_trx *trx);
-
-int sysmobts_get_target_power(struct gsm_bts_trx *trx);
-
-void sysmobts_pa_pwr_init(struct gsm_bts_trx *trx);
-void sysmobts_pa_maybe_step(struct gsm_bts_trx *trx);
 #endif
