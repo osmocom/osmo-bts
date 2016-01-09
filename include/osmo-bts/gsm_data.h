601781f463d0f5798ac27fd90be63d9f15261683
Harald Welte <laforge@gnumonks.org>
2016-01-09 17:56:10 +0100

    move 'GSM_FR_BYTES' and related definitiions to common part

--- include/osmo-bts/gsm_data.h
+++ include/osmo-bts/gsm_data.h
@@ -8,6 +8,13 @@
 #include <osmo-bts/paging.h>
 #include <osmo-bts/tx_power.h>
 
+#define GSM_FR_BITS	260
+#define GSM_EFR_BITS	244
+
+#define GSM_FR_BYTES	33	/* TS 101318 Chapter 5.1: 260 bits + 4bit sig */
+#define GSM_HR_BYTES	14	/* TS 101318 Chapter 5.2: 112 bits, no sig */
+#define GSM_EFR_BYTES	31	/* TS 101318 Chapter 5.3: 244 bits + 4bit sig */
+
 #define GSM_BTS_AGCH_QUEUE_THRESH_LEVEL_DEFAULT 41
 #define GSM_BTS_AGCH_QUEUE_THRESH_LEVEL_DISABLE 999999
 #define GSM_BTS_AGCH_QUEUE_LOW_LEVEL_DEFAULT 41
