osmocom/osmo-bts

commit 36153239bfd0b633c4d259a9de0a759f41197a2b

Remove duplicated code
Remove lchan deactivation related code duplication to facilitate future
use for dynamic CCCH re-activation.

Change-Id: Id0d3b19dbfaa16d1734321a07a6eb0355bfd77c9

src/osmo-bts-trx/l1_if.c
@@ -144,6 +144,23 @@ int check_transceiver_availability(struct gsm_bts *bts, int avail)
 	return 0;
 }
 
+int bts_model_lchan_deactivate(struct gsm_lchan *lchan)
+{
+	struct phy_instance *pinst = trx_phy_instance(lchan->ts->trx);
+	struct trx_l1h *l1h = pinst->u.osmotrx.hdl;
+
+	/* set lchan inactive */
+	lchan_set_state(lchan, LCHAN_S_NONE);
+
+	return trx_sched_set_lchan(&l1h->l1s, lchan->nr, LID_DEDIC, 0);
+}
+
+int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan)
+{
+	struct phy_instance *pinst = trx_phy_instance(lchan->ts->trx);
+	struct trx_l1h *l1h = pinst->u.osmotrx.hdl;
+	return trx_sched_set_lchan(&l1h->l1s, lchan->nr, LID_SACCH, 0);
+}
 
 /*
  * transceiver provisioning
@@ -646,16 +663,13 @@ int bts_model_l1sap_down(struct gsm_bts_trx *trx, struct osmo_phsap_prim *l1sap)
 				break;
 			}
 			/* deactivate associated channel */
-			trx_sched_set_lchan(&l1h->l1s, chan_nr, LID_SACCH, 0);
+			bts_model_lchan_deactivate_sacch(lchan);
 			if (!l1sap->u.info.u.act_req.sacch_only) {
-				/* set lchan inactive */
-				lchan_set_state(lchan, LCHAN_S_NONE);
 				/* deactivate dedicated channel */
-				trx_sched_set_lchan(&l1h->l1s, chan_nr, LID_DEDIC, 0);
+				lchan_deactivate(lchan);
 				/* confirm only on dedicated channel */
 				mph_info_chan_confirm(l1h, chan_nr,
 					PRIM_INFO_DEACTIVATE, 0);
-				lchan->ciph_state = 0; /* FIXME: do this in common/\*.c */
 			}
 			break;
 		default:

include/osmo-bts/bts_model.h
@@ -45,6 +45,9 @@ int bts_model_adjst_ms_pwr(struct gsm_lchan *lchan);
 
 int bts_model_l1sap_down(struct gsm_bts_trx *trx, struct osmo_phsap_prim *l1sap);
 
+int bts_model_lchan_deactivate(struct gsm_lchan *lchan);
+int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan);
+
 void bts_model_abis_close(struct gsm_bts *bts);
 
 int bts_model_ctrl_cmds_install(struct gsm_bts *bts);

include/osmo-bts/rsl.h
@@ -26,6 +26,8 @@ int rsl_tx_conn_fail(struct gsm_lchan *lchan, uint8_t cause);
 int rsl_tx_rf_rel_ack(struct gsm_lchan *lchan);
 int rsl_tx_hando_det(struct gsm_lchan *lchan, uint8_t *ho_delay);
 
+int lchan_deactivate(struct gsm_lchan *lchan);
+
 /* call-back for LAPDm code, called when it wants to send msgs UP */
 int lapdm_rll_tx_cb(struct msgb *msg, struct lapdm_entity *le, void *ctx);
 

src/common/rsl.c
@@ -2448,6 +2448,12 @@ static int rsl_rx_ipaccess(struct gsm_bts_trx *trx, struct msgb *msg)
 	return ret;
 }
 
+int lchan_deactivate(struct gsm_lchan *lchan)
+{
+	lchan->ciph_state = 0;
+	return bts_model_lchan_deactivate(lchan);
+}
+
 int down_rsl(struct gsm_bts_trx *trx, struct msgb *msg)
 {
 	struct abis_rsl_common_hdr *rslh = msgb_l2(msg);

src/osmo-bts-litecell15/lc15bts_vty.c
@@ -46,15 +46,14 @@
 #include <osmo-bts/gsm_data.h>
 #include <osmo-bts/phy_link.h>
 #include <osmo-bts/logging.h>
+#include <osmo-bts/bts_model.h>
 #include <osmo-bts/vty.h>
 
 #include "lc15bts.h"
 #include "l1_if.h"
 #include "utils.h"
 
-
 extern int lchan_activate(struct gsm_lchan *lchan);
-extern int lchan_deactivate(struct gsm_lchan *lchan);
 
 #define TRX_STR "Transceiver related commands\n" "TRX number\n"
 

src/osmo-bts-litecell15/oml.c
@@ -1656,10 +1656,9 @@ static void enqueue_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-int lchan_deactivate(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate(struct gsm_lchan *lchan)
 {
 	lchan_set_state(lchan, LCHAN_S_REL_REQ);
-	lchan->ciph_state = 0; /* FIXME: do this in common/\*.c */
 	enqueue_rel_marker(lchan);
 	return 0;
 }
@@ -1674,7 +1673,7 @@ static void enqueue_sacch_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-static int lchan_deactivate_sacch(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan)
 {
 	enqueue_sacch_rel_marker(lchan);
 	return 0;
@@ -1858,7 +1857,7 @@ int l1if_rsl_deact_sacch(struct gsm_lchan *lchan)
 	/* Only de-activate the SACCH if the lchan is active */
 	if (lchan->state != LCHAN_S_ACTIVE)
 		return 0;
-	return lchan_deactivate_sacch(lchan);
+	return bts_model_lchan_deactivate_sacch(lchan);
 }
 
 int bts_model_trx_deact_rf(struct gsm_bts_trx *trx)

src/osmo-bts-octphy/l1_oml.c
@@ -884,7 +884,7 @@ static void enqueue_sacch_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-static int lchan_deactivate_sacch(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan)
 {
 	enqueue_sacch_rel_marker(lchan);
 	return 0;
@@ -895,7 +895,7 @@ int l1if_rsl_deact_sacch(struct gsm_lchan *lchan)
 	/* Only de-activate the SACCH if the lchan is active */
 	if (lchan->state != LCHAN_S_ACTIVE)
 		return 0;
-	return lchan_deactivate_sacch(lchan);
+	return bts_model_lchan_deactivate_sacch(lchan);
 }
 
 
@@ -913,10 +913,9 @@ static void enqueue_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-static int lchan_deactivate(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate(struct gsm_lchan *lchan)
 {
 	lchan_set_state(lchan, LCHAN_S_REL_REQ);
-	lchan->ciph_state = 0;	/* FIXME: do this in common *.c */
 	enqueue_rel_marker(lchan);
 	return 0;
 }

src/osmo-bts-sysmo/oml.c
@@ -1676,10 +1676,9 @@ static void enqueue_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-int lchan_deactivate(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate(struct gsm_lchan *lchan)
 {
 	lchan_set_state(lchan, LCHAN_S_REL_REQ);
-	lchan->ciph_state = 0; /* FIXME: do this in common/\*.c */
 	enqueue_rel_marker(lchan);
 	return 0;
 }
@@ -1694,7 +1693,7 @@ static void enqueue_sacch_rel_marker(struct gsm_lchan *lchan)
 	queue_sapi_command(lchan, cmd);
 }
 
-static int lchan_deactivate_sacch(struct gsm_lchan *lchan)
+int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan)
 {
 	enqueue_sacch_rel_marker(lchan);
 	return 0;
@@ -1878,7 +1877,7 @@ int l1if_rsl_deact_sacch(struct gsm_lchan *lchan)
 	/* Only de-activate the SACCH if the lchan is active */
 	if (lchan->state != LCHAN_S_ACTIVE)
 		return 0;
-	return lchan_deactivate_sacch(lchan);
+	return bts_model_lchan_deactivate_sacch(lchan);
 }
 
 int bts_model_trx_deact_rf(struct gsm_bts_trx *trx)

commit 2c95ae6a091083a7d1108aa852d6bfacd442f62d

osmo-bts-trx: fix lchan deactivation
Use chan_nr for deactivating lchan instead of lchan->nr: chan_nr is the
RSL Channel Number IE value, a bitfield aggregation of lchan type
bits (cbits) and lchan number (lowest three bits). The error was
introduced in 36153239bfd0b633c4d259a9de0a759f41197a2b.

Change-Id: I6dd7060422ab9d18743c1ff2ab419e3e7299d74d

src/osmo-bts-trx/l1_if.c
@@ -155,14 +155,16 @@ int bts_model_lchan_deactivate(struct gsm_lchan *lchan)
 	/* set lchan inactive */
 	lchan_set_state(lchan, LCHAN_S_NONE);
 
-	return trx_sched_set_lchan(&l1h->l1s, lchan->nr, LID_DEDIC, 0);
+	return trx_sched_set_lchan(&l1h->l1s, gsm_lchan2chan_nr(lchan),
+				   LID_DEDIC, 0);
 }
 
 int bts_model_lchan_deactivate_sacch(struct gsm_lchan *lchan)
 {
 	struct phy_instance *pinst = trx_phy_instance(lchan->ts->trx);
 	struct trx_l1h *l1h = pinst->u.osmotrx.hdl;
-	return trx_sched_set_lchan(&l1h->l1s, lchan->nr, LID_SACCH, 0);
+	return trx_sched_set_lchan(&l1h->l1s, gsm_lchan2chan_nr(lchan),
+				   LID_SACCH, 0);
 }
 
 /*