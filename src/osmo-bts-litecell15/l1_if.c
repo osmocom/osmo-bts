2a4c5fa2b388e6395b0e5d44bbf7ee60c7a82bcb
Minh-Quang Nguyen <minh-quang.nguyen@nutaq.com>
2017-06-24 01:08:30 +0200

    LC15: properly handle BS-AG-BLKS-RES as received from BSC
    Closes: OS#2014
    Change-Id: Icf437e621d1991f6185e31a0953773309e2bf5ac

--- src/osmo-bts-litecell15/l1_if.c
+++ src/osmo-bts-litecell15/l1_if.c
@@ -389,7 +389,7 @@ static int ph_data_req(struct gsm_bts_trx *trx, struct msgb *msg,
 		/* The sapi depends on DSP configuration, not
 		 * on the actual SYSTEM INFORMATION 3. */
 		u8BlockNbr = L1SAP_FN2CCCHBLOCK(u32Fn);
-		if (u8BlockNbr >= 1)
+		if (u8BlockNbr >= num_agch(trx, "PH-DATA-REQ"))
 			sapi = GsmL1_Sapi_Pch;
 		else
 			sapi = GsmL1_Sapi_Agch;
