osmocom/osmo-bts

commit 44a35902adde70b4e4696640f09f448ba44d51d2

bts.c: Log name of RR msg type instead of value
Change-Id: Ia35e132e0b6532dfbf09bb33fe9328a9e3e16885

src/common/bts.c
@@ -538,8 +538,8 @@ int bts_agch_enqueue(struct gsm_bts *bts, struct msgb *msg)
 	if (bts->agch_queue.length > hard_limit) {
 		LOGP(DSUM, LOGL_ERROR,
 		     "AGCH: too many messages in queue, "
-		     "refusing message type 0x%02x, length = %d/%d\n",
-		     ((struct gsm48_imm_ass *)msgb_l3(msg))->msg_type,
+		     "refusing message type %s, length = %d/%d\n",
+		     gsm48_rr_msg_name(((struct gsm48_imm_ass *)msgb_l3(msg))->msg_type),
 		     bts->agch_queue.length, bts->agch_queue.max_length);
 
 		bts->agch_queue.rejected_msgs++;

commit ef1356c06ccfde74ceeeed4fd9df16d6e09fdf32

bts.c: Add missing include for gsm48_rr_msg_name
It seems I somehow mischecked that the required include for gsm48_rr_msg_name
was not yet present in the file.

Fixes: 44a35902adde70b4e4696640f09f448ba44d51d2
Change-Id: Ia82bc6a5ff7e3989cf0b85ee689d0c3344720eda

src/common/bts.c
@@ -35,6 +35,7 @@
 #include <osmocom/core/stats.h>
 #include <osmocom/core/rate_ctr.h>
 #include <osmocom/gsm/protocol/gsm_12_21.h>
+#include <osmocom/gsm/gsm48.h>
 #include <osmocom/gsm/lapdm.h>
 #include <osmocom/trau/osmo_ortp.h>
 