osmocom/osmo-bts

commit eebb6a4216f136132400831ca35ed70e7502e92a

bts: Allocate TRX for BTS dynamically, deprecate -t
No need to pass -t num_trx anymore to specify number of TRX to use. It
is calculated based on dynamic allocation from VTY config.
Using parameter -t is flagged as deprecated and is transformed into a
NOOP por backward compatibility.

As a result, TRX now are allocated after the BTS is allocated and
initial config (pre-VTY) is applied.
A new function bts_trx_init() is added, to set default config on each
TRX during allocation and before setting VTY config on it.
A new per BTS model function bts_model_trx_init() is added, to allow
per model specific default configuration of each TRX.

Change-Id: Iab1a754ab12a626759f9f90aa66f87bdce65ac9c

src/common/bts.c
@@ -105,11 +105,10 @@ static const struct rate_ctr_group_desc bts_ctrg_desc = {
 	bts_ctr_desc
 };
 
-/* Initialize the BTS (and TRX) data structures, called before config
+/* Initialize the BTS data structures, called before config
  * file reading */
 int bts_init(struct gsm_bts *bts)
 {
-	struct gsm_bts_trx *trx;
 	int rc, i;
 	static int initialized = 0;
 	void *tall_rtp_ctx;
@@ -167,26 +166,6 @@ int bts_init(struct gsm_bts *bts)
 	oml_mo_state_init(&bts->gprs.nsvc[0].mo, -1, NM_AVSTATE_DEPENDENCY);
 	oml_mo_state_init(&bts->gprs.nsvc[1].mo, NM_OPSTATE_DISABLED, NM_AVSTATE_OFF_LINE);
 
-	/* initialize bts data structure */
-	llist_for_each_entry(trx, &bts->trx_list, list) {
-		struct trx_power_params *tpp = &trx->power_params;
-		int i;
-
-		for (i = 0; i < ARRAY_SIZE(trx->ts); i++) {
-			struct gsm_bts_trx_ts *ts = &trx->ts[i];
-			int k;
-
-			for (k = 0; k < ARRAY_SIZE(ts->lchan); k++) {
-				struct gsm_lchan *lchan = &ts->lchan[k];
-				INIT_LLIST_HEAD(&lchan->dl_tch_queue);
-			}
-		}
-		/* Default values for the power adjustments */
-		tpp->ramp.max_initial_pout_mdBm = to_mdB(0);
-		tpp->ramp.step_size_mdB = to_mdB(2);
-		tpp->ramp.step_interval_sec = 1;
-	}
-
 	/* allocate a talloc pool for ORTP to ensure it doesn't have to go back
 	 * to the libc malloc all the time */
 	tall_rtp_ctx = talloc_pool(tall_bts_ctx, 262144);
@@ -213,6 +192,36 @@ int bts_init(struct gsm_bts *bts)
 	OSMO_ASSERT(rc == 0);
 
 	return rc;
+}
+
+/* Initialize the TRX data structures, called before config
+ * file reading */
+int bts_trx_init(struct gsm_bts_trx *trx)
+{
+	/* initialize bts data structure */
+	struct trx_power_params *tpp = &trx->power_params;
+	int rc, i;
+
+	for (i = 0; i < ARRAY_SIZE(trx->ts); i++) {
+		struct gsm_bts_trx_ts *ts = &trx->ts[i];
+		int k;
+
+		for (k = 0; k < ARRAY_SIZE(ts->lchan); k++) {
+			struct gsm_lchan *lchan = &ts->lchan[k];
+			INIT_LLIST_HEAD(&lchan->dl_tch_queue);
+		}
+	}
+	/* Default values for the power adjustments */
+	tpp->ramp.max_initial_pout_mdBm = to_mdB(0);
+	tpp->ramp.step_size_mdB = to_mdB(2);
+	tpp->ramp.step_interval_sec = 1;
+
+	rc = bts_model_trx_init(trx);
+	if (rc < 0) {
+		llist_del(&trx->list);
+		return rc;
+	}
+	return 0;
 }
 
 static void shutdown_timer_cb(void *data)

src/common/vty.c
@@ -227,12 +227,22 @@ DEFUN(cfg_bts_trx, cfg_bts_trx_cmd,
 	struct gsm_bts *bts = vty->index;
 	struct gsm_bts_trx *trx;
 
-	trx = gsm_bts_trx_num(bts, trx_nr);
+
+	if (trx_nr > bts->num_trx) {
+		vty_out(vty, "%% The next unused TRX number is %u%s",
+			bts->num_trx, VTY_NEWLINE);
+		return CMD_WARNING;
+	} else if (trx_nr == bts->num_trx) {
+		/* allocate a new one */
+		trx = gsm_bts_trx_alloc(bts);
+		if (trx)
+			bts_trx_init(trx);
+	} else
+		trx = gsm_bts_trx_num(bts, trx_nr);
+
 	if (!trx) {
-		vty_out(vty, "Unknown TRX %u. Available TRX are: 0..%u%s",
-			trx_nr, bts->num_trx - 1, VTY_NEWLINE);
-		vty_out(vty, "Hint: Check if commandline option -t matches the"
-			"number of available transceivers!%s", VTY_NEWLINE);
+		vty_out(vty, "%% Unable to allocate TRX %u%s",
+			trx_nr, VTY_NEWLINE);
 		return CMD_WARNING;
 	}
 

include/osmo-bts/bts.h
@@ -27,6 +27,7 @@ enum {
 extern void *tall_bts_ctx;
 
 int bts_init(struct gsm_bts *bts);
+int bts_trx_init(struct gsm_bts_trx *trx);
 void bts_shutdown(struct gsm_bts *bts, const char *reason);
 
 struct gsm_bts *create_bts(uint8_t num_trx, char *id);

include/osmo-bts/bts_model.h
@@ -14,6 +14,7 @@ struct phy_instance;
 /* BTS model specific functions needed by the common code */
 
 int bts_model_init(struct gsm_bts *bts);
+int bts_model_trx_init(struct gsm_bts_trx *trx);
 
 int bts_model_check_oml(struct gsm_bts *bts, uint8_t msg_type,
 			struct tlv_parsed *old_attr, struct tlv_parsed *new_attr,

src/common/main.c
@@ -59,7 +59,6 @@ int quit = 0;
 static const char *config_file = "osmo-bts.cfg";
 static int daemonize = 0;
 static int rt_prio = -1;
-static int trx_num = 1;
 static char *gsmtap_ip = 0;
 extern int g_vty_port_num;
 
@@ -76,8 +75,6 @@ static void print_help()
 		"  -e 	--log-level	Set a global log-level\n"
 		"  -r	--realtime PRIO	Use SCHED_RR with the specified priority\n"
 		"  -i	--gsmtap-ip	The destination IP used for GSMTAP.\n"
-		"  -t	--trx-num	Set number of TRX (default=%d)\n",
-		trx_num
 		);
 	bts_model_print_help();
 }
@@ -152,9 +149,8 @@ static void handle_options(int argc, char **argv)
 			gsmtap_ip = optarg;
 			break;
 		case 't':
-			trx_num = atoi(optarg);
-			if (trx_num < 1)
-				trx_num = 1;
+			fprintf(stderr, "Parameter -t is deprecated and does nothing, "
+					"TRX num is calculated from VTY\n");
 			break;
 		case '?':
 		case 1:
@@ -228,7 +224,7 @@ int bts_main(int argc, char **argv)
 {
 	struct gsm_bts_trx *trx;
 	struct e1inp_line *line;
-	int rc, i;
+	int rc;
 
 	printf("((*))\n  |\n / \\ OsmoBTS\n");
 
@@ -251,13 +247,7 @@ int bts_main(int argc, char **argv)
 		fprintf(stderr, "Failed to create BTS structure\n");
 		exit(1);
 	}
-	for (i = 1; i < trx_num; i++) {
-		trx = gsm_bts_trx_alloc(bts);
-		if (!trx) {
-			fprintf(stderr, "Failed to create TRX structure\n");
-			exit(1);
-		}
-	}
+
 	e1inp_vty_init();
 	bts_vty_init(bts, &bts_log_info);
 

src/osmo-bts-litecell15/main.c
@@ -79,11 +79,6 @@ int bts_model_init(struct gsm_bts *bts)
 		exit(1);
 	}
 
-	llist_for_each_entry(trx, &bts->trx_list, list) {
-		trx->nominal_power = 40;
-		trx->power_params.trx_p_max_out_mdBm = to_mdB(bts->c0->nominal_power);
-	}
-
 	if (stat(LC15BTS_RF_LOCK_PATH, &st) == 0) {
 		LOGP(DL1C, LOGL_NOTICE, "Not starting BTS due to RF_LOCK file present\n");
 		exit(23);
@@ -101,6 +96,13 @@ int bts_model_init(struct gsm_bts *bts)
 
 	bts_model_vty_init(bts);
 
+	return 0;
+}
+
+int bts_model_trx_init(struct gsm_bts_trx *trx)
+{
+	trx->nominal_power = 40;
+	trx->power_params.trx_p_max_out_mdBm = to_mdB(trx->bts->c0->nominal_power);
 	return 0;
 }
 

src/osmo-bts-oc2g/main.c
@@ -105,11 +105,6 @@ int bts_model_init(struct gsm_bts *bts)
 		exit(1);
 	}
 
-	llist_for_each_entry(trx, &bts->trx_list, list) {
-		trx->nominal_power = 40;
-		trx->power_params.trx_p_max_out_mdBm = to_mdB(bts->c0->nominal_power);
-	}
-
 	if (stat(OC2GBTS_RF_LOCK_PATH, &st) == 0) {
 		LOGP(DL1C, LOGL_NOTICE, "Not starting BTS due to RF_LOCK file present\n");
 		exit(23);
@@ -127,6 +122,13 @@ int bts_model_init(struct gsm_bts *bts)
 
 	bts_model_vty_init(bts);
 
+	return 0;
+}
+
+int bts_model_trx_init(struct gsm_bts_trx *trx)
+{
+	trx->nominal_power = 40;
+	trx->power_params.trx_p_max_out_mdBm = to_mdB(trx->bts->c0->nominal_power);
 	return 0;
 }
 

src/osmo-bts-octphy/l1_if.c
@@ -792,6 +792,11 @@ int bts_model_init(struct gsm_bts *bts)
 	return 0;
 }
 
+int bts_model_trx_init(struct gsm_bts_trx *trx)
+{
+	return 0;
+}
+
 /***********************************************************************
  * handling of messages coming up from PHY
  ***********************************************************************/

commit 6dacc35c751452f0e94acf15e6fd42ac774c8e53

bts: Fix TRX0 param initialization
TRX0 is allocated in a different place than other TRX.

Commit that introduced the bug added an initialization step
(bts_trx_init) applied in VTY cmd "trx <0-254>", where TRX are allocated
since same commit, but doesn't initialize TRX0 for reasons explained in
first paragraph.

As a result, some parameters were not applied to TRX0, like
power ramping increment step, which prevented MS from finding the network in
osmo-gsm-tester tests.

Fixes: eebb6a4216f136132400831ca35ed70e7502e92a ("bts: Allocate TRX for BTS dynamically, deprecate -t")
Change-Id: I5afd66548634ff53e2af24dc7055d408ca8ca4e3

src/common/bts.c
@@ -177,6 +177,8 @@ int bts_init(struct gsm_bts *bts)
 		return rc;
 	}
 
+	/* TRX0 was allocated early during gsm_bts_alloc, not later through VTY */
+	bts_trx_init(bts->c0);
 	bts_gsmnet.num_bts++;
 
 	if (!initialized) {

src/common/gsm_data_shared.c
@@ -333,7 +333,7 @@ struct gsm_bts *gsm_bts_alloc(void *ctx, uint8_t bts_num)
 	memcpy(&bts->gprs.cell.rlc_cfg, &rlc_cfg_default,
 		sizeof(bts->gprs.cell.rlc_cfg));
 
-	/* create our primary TRX */
+	/* create our primary TRX. It will be initialized during bts_init() */
 	bts->c0 = gsm_bts_trx_alloc(bts);
 	if (!bts->c0) {
 		talloc_free(bts);

src/common/vty.c
@@ -233,7 +233,10 @@ DEFUN(cfg_bts_trx, cfg_bts_trx_cmd,
 			bts->num_trx, VTY_NEWLINE);
 		return CMD_WARNING;
 	} else if (trx_nr == bts->num_trx) {
-		/* allocate a new one */
+		/* Allocate a new TRX
+		 * Remark: TRX0 was already created during gsm_bts_alloc() and
+		 * 	   initialized in bts_init(), not here.
+		 */
 		trx = gsm_bts_trx_alloc(bts);
 		if (trx)
 			bts_trx_init(trx);