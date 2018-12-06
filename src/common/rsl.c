osmocom/osmo-bts

commit 133a3d96dc07ebda4dfc7899dab9c0d0c80c9fea

rsl: Avoid sending ipa PDCH DEACT NACK followed by ACK
It was spotted during osmo-gsm-tester test
dynts:trx-sysmocell5000+mod-bts0-dynts67-ipa+cfg-codec-fr-any that
osmo-bts-trx was answering to PDCH DEACT from BSC first with a NACK
followed immediatelly after by an ACK. That happens after the test does
a GPRS pdp ctx act successfuly and then deactivates the ctx and the 2 MS
try to place a call between them (and thus channels need to be moved to
TCH/F).

Most probably the if condition where the lines for this commit are
modified need to be fine-grained. Patch before this one should help to
understand the steates/situation in this scenario, and then a follow-up
patch can be created to improve the logic.

Change-Id: I91c65da6b6b7094f32187d3b083153a87c3219fd

src/common/rsl.c
@@ -2200,11 +2200,10 @@ static void rsl_rx_dyn_pdch(struct msgb *msg, bool pdch_act)
 	}
 
 	if (lchan->state != LCHAN_S_NONE) {
-		LOGP(DRSL, LOGL_ERROR,
+		LOGP(DRSL, LOGL_NOTICE,
 		     "%s Request to PDCH %s, but lchan is still in state %s\n",
 		     gsm_ts_and_pchan_name(ts), pdch_act? "ACT" : "DEACT",
 		     gsm_lchans_name(lchan->state));
-		rsl_tx_dyn_pdch_nack(lchan, pdch_act, RSL_ERR_NORMAL_UNSPEC);
 	}
 
 	ts->flags |= pdch_act? TS_F_PDCH_ACT_PENDING

commit 21a8fb53af19c77c481e1aa31442390600eaa19c

rsl: Send PDCH ACT NACK if TCH chan is still active
Fix recent commit which broke TTCN3 BTS_tests
TC_dyn_ipa_pdch_tchf_act_pdch_act_nack.

Prior to the breaking commit, logic was still not good, because
1- It didn't return after sending the NACK
2- It sent a NACK in all cases, while for PDCH DEACT we want to force
its deactivation. Going through tests it can be seen that indeed it
can deactivate it in that case:
rsl.c:2206 (bts=0,trx=0,ts=3,pchan=TCH/F_PDCH as PDCH) Request to PDCH DEACT, but lchan is still in state ACTIVE
...
rsl.c:2103 (bts=0,trx=0,ts=3,ss=0) Tx PDCH DEACT ACK

Fixes: 133a3d96dc07ebda4dfc7899dab9c0d0c80c9fea ("rsl: Avoid sending ipa PDCH DEACT NACK followed by ACK")
Change-Id: I6d6d12aec10c801fe55012ca6e58d0bc8755b15d

src/common/rsl.c
@@ -2204,6 +2204,12 @@ static void rsl_rx_dyn_pdch(struct msgb *msg, bool pdch_act)
 		     "%s Request to PDCH %s, but lchan is still in state %s\n",
 		     gsm_ts_and_pchan_name(ts), pdch_act? "ACT" : "DEACT",
 		     gsm_lchans_name(lchan->state));
+		/* TCH takes preference over PDCH so allow forcing PDCH DEACT,
+		 * but forbid forcing PDCH ACT if lchan still active */
+		if (pdch_act) {
+			rsl_tx_dyn_pdch_nack(lchan, pdch_act, RSL_ERR_NORMAL_UNSPEC);
+			return;
+		}
 	}
 
 	ts->flags |= pdch_act? TS_F_PDCH_ACT_PENDING