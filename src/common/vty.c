f9115ae67adcc8ca06265cff21fc83e82f06d6b5
Vadim Yanitskiy <axilirator@gmail.com>
2019-11-30 06:18:26 +0700

    common/vty.c: get rid of generic exit / end commands
    Those commands are now handled by libosmovty itself.
    
    Change-Id: I425f9058ae15de929e2ba0283d4057bdf767aeeb

--- src/common/vty.c
+++ src/common/vty.c
@@ -125,48 +125,6 @@ int bts_vty_is_config_node(struct vty *vty, int node)
 	}
 }
 
-gDEFUN(ournode_exit, ournode_exit_cmd, "exit",
-	"Exit current node, go down to provious node")
-{
-	switch (vty->node) {
-	case PHY_INST_NODE:
-		vty->node = PHY_NODE;
-		{
-			struct phy_instance *pinst = vty->index;
-			vty->index = pinst->phy_link;
-		}
-		break;
-	case PHY_NODE:
-		vty->node = CONFIG_NODE;
-		vty->index = NULL;
-		break;
-	case TRX_NODE:
-		vty->node = BTS_NODE;
-		{
-			struct gsm_bts_trx *trx = vty->index;
-			vty->index = trx->bts;
-		}
-		break;
-	default:
-		break;
-	}
-	return CMD_SUCCESS;
-}
-
-gDEFUN(ournode_end, ournode_end_cmd, "end",
-	"End current mode and change to enable mode")
-{
-	switch (vty->node) {
-	default:
-		vty_config_unlock(vty);
-		vty->node = ENABLE_NODE;
-		vty->index = NULL;
-		vty->index_sub = NULL;
-		break;
-	}
-	return CMD_SUCCESS;
-}
-
 static const char osmobts_copyright[] =
 	"Copyright (C) 2010, 2011 by Harald Welte, Andreas Eversberg and On-Waves\r\n"
 	"License AGPLv3+: GNU AGPL version 3 or later <http://gnu.org/licenses/agpl-3.0.html>\r\n"
