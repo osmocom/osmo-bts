d4a53017e1baab23a9c52aa807d78a6c28522e85
Pau Espin Pedrol <pespin@sysmocom.de>
2020-05-18 10:49:03 +0200

    l1_utils.h: Avoid redefinition of global vars defined in l1_utils.c
    Change-Id: Idd40433fee22aeb1e86708c132929bb69e626334

--- src/osmo-bts-octphy/l1_utils.h
+++ src/osmo-bts-octphy/l1_utils.h
@@ -2,8 +2,8 @@
 
 #include <osmocom/core/utils.h>
 
-const struct value_string octphy_l1sapi_names[23];
-const struct value_string octphy_dir_names[5];
-const struct value_string octphy_clkmgr_state_vals[8];
-const struct value_string octphy_cid_vals[37];
-const struct value_string octphy_eid_vals[7];
+extern const struct value_string octphy_l1sapi_names[23];
+extern const struct value_string octphy_dir_names[5];
+extern const struct value_string octphy_clkmgr_state_vals[8];
+extern const struct value_string octphy_cid_vals[37];
+extern const struct value_string octphy_eid_vals[7];
