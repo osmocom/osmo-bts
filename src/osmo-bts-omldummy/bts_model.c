7dd865baddc6c4cf66789cddbfbed933d3e3babf
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2020-10-03 06:34:18 +0700

    osmo-bts-omldummy: fix: do not crash on OML connection drop/failure
    This regression was introduced in 2ff4592ffc78649bb5c14a213a74569b43da255b.
    There is simply no PHY in case of osmo-bts-omldummy.
    
    Change-Id: I864ac3f15e06462d6ce808b3f2188c5c39a5aad2
    Fixes: Ic00df9e7278d42bc10c1e1a1c0edde7e13199299

--- src/osmo-bts-omldummy/bts_model.c
+++ src/osmo-bts-omldummy/bts_model.c
@@ -52,9 +52,6 @@ int osmo_amr_rtp_dec(const uint8_t *rtppayload, int payload_len, uint8_t *cmr,
 
 void bts_model_trx_close(struct gsm_bts_trx *trx)
 {
-	struct phy_instance *pinst = trx_phy_instance(trx);
-	phy_link_state_set(pinst->phy_link, PHY_LINK_SHUTDOWN);
-
 	bts_model_trx_close_cb(trx, 0);
 }
 
