43f211a77d13b6f8039a88c7616383874857b87a
Vadim Yanitskiy <vyanitskiy@sysmocom.de>
2020-10-13 23:04:05 +0700

    tests/power_test: also match stderr, not only stdout
    Change-Id: I68e749e9f5c15197b1de4a2aec40338e590553b0

--- tests/power/power_test.c
+++ tests/power/power_test.c
@@ -130,6 +130,10 @@ int main(int argc, char **argv)
 	msgb_talloc_ctx_init(tall_bts_ctx, 0);
 
 	osmo_init_logging2(tall_bts_ctx, &bts_log_info);
+	osmo_stderr_target->categories[DLOOP].loglevel = LOGL_DEBUG;
+	osmo_stderr_target->categories[DL1C].loglevel = LOGL_DEBUG;
+	log_set_print_filename(osmo_stderr_target, 0);
+	log_set_use_color(osmo_stderr_target, 0);
 
 	test_power_loop();
 
