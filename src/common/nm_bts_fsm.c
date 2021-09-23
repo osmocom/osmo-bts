osmocom/osmo-bts

commit 4ddc37ce7140e5ce6ca6cce5aeeefedc0d395d22

nm_bts_fsm: Make sure PHYs are opened when SW_ACTivating it
Since recently we support shutting down phys when BTS goes into shutdown
mode. Let's make sure they are opened again when we connect again to the
BSC.

Change-Id: Ia1df6f4a1e0e6daeffe7303d518776a04b023930

src/common/nm_bts_fsm.c
@@ -65,10 +65,23 @@ static void st_op_disabled_notinstalled_on_enter(struct osmo_fsm_inst *fi, uint3
 static void st_op_disabled_notinstalled(struct osmo_fsm_inst *fi, uint32_t event, void *data)
 {
 	struct gsm_bts *bts = (struct gsm_bts *)fi->priv;
+	struct gsm_bts_trx *trx;
 
 	switch (event) {
 	case NM_EV_SW_ACT:
 		oml_mo_tx_sw_act_rep(&bts->mo);
+
+		llist_for_each_entry(trx, &bts->trx_list, list) {
+			/* During startup, phy_links are already opened, but if we are
+			 * re-connecting, phy_link was closed when disconnected from
+			 * previous BSC, so let's re-open it.
+			 */
+			struct phy_instance *pinst = trx_phy_instance(trx);
+			struct phy_link *plink = pinst->phy_link;
+			if (phy_link_state_get(plink) == PHY_LINK_SHUTDOWN)
+				bts_model_phy_link_open(plink);
+		}
+
 		nm_bts_fsm_state_chg(fi, NM_BTS_ST_OP_DISABLED_OFFLINE);
 		return;
 	default:

commit 72d81b978a0d0429f07c1bb67feb54973ad6fe13

osmo-bts-omldummy: Fix crash accessing null phy
osmo-bts-omldummy is known to be specially in the sense that it has no
phy. This should ideally be changed to have some dummy object, but so
far that's how it is. A recent commit checking the phy's forgot about
the fact that BTS model has no phy. Let's fix it.

Fixes: 4ddc37ce7140e5ce6ca6cce5aeeefedc0d395d22
Change-Id: Iba7873530aac74b3bf799819890641196dacf170

src/common/nm_bts_fsm.c
@@ -72,6 +72,8 @@ static void st_op_disabled_notinstalled(struct osmo_fsm_inst *fi, uint32_t event
 		oml_mo_tx_sw_act_rep(&bts->mo);
 
 		llist_for_each_entry(trx, &bts->trx_list, list) {
+			if (trx->bts->variant == BTS_OSMO_OMLDUMMY) /* In OMLDUMMY, phy=NULL */
+				continue;
 			/* During startup, phy_links are already opened, but if we are
 			 * re-connecting, phy_link was closed when disconnected from
 			 * previous BSC, so let's re-open it.