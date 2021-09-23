osmocom/osmo-bts

commit c2ba34d9c13d309926c760943a9531fd3b236189

abis: Clear code and drop code not executed
abis_init() sets "g_bts = bts;", hence bts object is the same as g_bts
(since we only have 1 BTS object in osmo-bts). As a result, dropping
checking first bts->... and later g_bts->... makes no sense: the later
condition will always be false.

Change-Id: I5018199221fb3c3329a59d0b91e827f077cef85d

src/common/abis.c
@@ -102,18 +102,12 @@ static void abis_link_connected(struct osmo_fsm_inst *fi, uint32_t event, void *
 			LOGP(DABIS, LOGL_FATAL, "OML link was closed early within %" PRIu64 " seconds. "
 			     "If this situation persists, please check your BTS and BSC configuration files for errors. "
 			     "A common error is a mismatch between unit_id configuration parameters of BTS and BSC.\n",
-			     (uint64_t) (now.tv_sec - g_bts->oml_conn_established_timestamp.tv_sec));
+			     (uint64_t) (now.tv_sec - bts->oml_conn_established_timestamp.tv_sec));
 		}
 		bts->oml_link = NULL;
 		oml_rsl_was_connected = true;
 	}
-	memset(&g_bts->oml_conn_established_timestamp, 0, sizeof(bts->oml_conn_established_timestamp));
-
-	if (g_bts->osmo_link) {
-		e1inp_sign_link_destroy(g_bts->osmo_link);
-		g_bts->osmo_link = NULL;
-		oml_rsl_was_connected = true;
-	}
+	memset(&bts->oml_conn_established_timestamp, 0, sizeof(bts->oml_conn_established_timestamp));
 
 	/* Then iterate over the RSL signalling links */
 	llist_for_each_entry(trx, &bts->trx_list, list) {

commit 127e8fc4166dc34e17dd0768028f3f00a5fa639c

abis: Fix memory leak of bts->osmo_link upon link going down
Commit below (see "Fixes" section) wrongly erased the code re-introduced
in this commit, due to not spotting different between "oml_link" and
"osmo_link". This commit is hence a revert of such commit, updated to
current code.

Fixes: c2ba34d9c13d309926c760943a9531fd3b236189
Change-Id: Id436116e5cd0bec024b2f9943fbff8d0bdc956ac

src/common/abis.c
@@ -102,6 +102,13 @@ static void reset_oml_link(struct gsm_bts *bts)
 		bts->oml_link = NULL;
 	}
 	memset(&bts->oml_conn_established_timestamp, 0, sizeof(bts->oml_conn_established_timestamp));
+
+	/* Same for IPAC_PROTO_OSMO on the same ipa connection: */
+	if (bts->osmo_link) {
+		e1inp_sign_link_destroy(bts->osmo_link);
+		bts->osmo_link = NULL;
+	}
+
 }
 
 static int pick_next_bsc(struct osmo_fsm_inst *fi)