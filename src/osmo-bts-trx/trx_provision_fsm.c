osmocom/osmo-bts

commit 32b51eca7d2a5215814db7843b1a0f8c74ea7527

trx_provision_fsm: Properly reset FSM state upon starting listening for events
Change-Id: If0d078bc0170091efbca351c44ee412dad1e944a

src/osmo-bts-trx/trx_provision_fsm.c
@@ -92,6 +92,57 @@ void l1if_setformat_cb(struct trx_l1h *l1h, int rc)
 /*
  * transceiver provisioning
  */
+
+static void trx_provision_reset(struct trx_l1h *l1h)
+{
+	struct phy_instance *pinst = l1h->phy_inst;
+	uint8_t tn;
+
+	l1h->config.trxd_pdu_ver_req = pinst->phy_link->u.osmotrx.trxd_pdu_ver_max;
+	l1h->config.trxd_pdu_ver_use = 0;
+	l1h->config.setformat_sent = false;
+	l1h->config.setformat_acked = false;
+
+	l1h->config.enabled = false;
+	l1h->config.arfcn_valid = false;
+	l1h->config.arfcn = 0;
+	l1h->config.rxtune_sent = false;
+	l1h->config.rxtune_acked = false;
+	l1h->config.txtune_sent = false;
+	l1h->config.txtune_acked = false;
+
+	l1h->config.tsc_valid = false;
+	l1h->config.tsc = 0;
+	l1h->config.tsc_sent = false;
+	l1h->config.tsc_acked = false;
+
+	l1h->config.bsic_valid = false;
+	l1h->config.bsic = 0;
+	l1h->config.bsic_sent = false;
+	l1h->config.bsic_acked = false;
+
+	l1h->config.rxgain_valid = false;
+	l1h->config.rxgain = 0;
+	l1h->config.rxgain_sent = false;
+
+	l1h->config.nomtxpower_sent = false;
+	l1h->config.nomtxpower_acked = false;
+
+	l1h->config.maxdly_valid = false;
+	l1h->config.maxdly_sent = false;
+
+	l1h->config.maxdlynb_valid = false;
+	l1h->config.maxdlynb_sent = false;
+
+	for (tn = 0; tn < TRX_NR_TS; tn++) {
+		l1h->config.setslot_valid[tn] = false;
+		l1h->config.setslot_sent[tn] = false;
+		l1h->config.setslot[tn].slottype = 0;
+		l1h->config.setslot[tn].tsc_set = 0;
+		l1h->config.setslot[tn].tsc_val = 0;
+		l1h->config.setslot[tn].tsc_valid = 0;
+	}
+}
 int l1if_provision_transceiver_trx(struct trx_l1h *l1h)
 {
 	struct phy_instance *pinst = l1h->phy_inst;
@@ -274,7 +325,7 @@ static void st_open_poweroff_on_enter(struct osmo_fsm_inst *fi, uint32_t prev_st
 	struct trx_l1h *l1h = (struct trx_l1h *)fi->priv;
 	struct phy_instance *pinst = l1h->phy_inst;
 
-	l1h->config.trxd_pdu_ver_req = pinst->phy_link->u.osmotrx.trxd_pdu_ver_max;
+	trx_provision_reset(l1h);
 
 	/* Apply initial RFMUTE state */
 	if (pinst->trx != NULL)

commit 5f4e7291222d0135d7609d7cd0c3da2fee5412d9

bts-trx: Fix rxgain & maxdly VTY values being reset
Recent commit added code to reset trx_provision_fsm state fields in orer
to support re-using the FSM once BTS is reconnected to a BSC.

However, some values being reset there are not set up when reconnecting
to the BSC, but rather configured through VTY at startup. Hence those
values should be kept.

Fixes: 32b51eca7d2a5215814db7843b1a0f8c74ea7527
Change-Id: I5c31241327b98386c313cd9377cf76c009456105

src/osmo-bts-trx/trx_provision_fsm.c
@@ -122,17 +122,13 @@ static void trx_provision_reset(struct trx_l1h *l1h)
 	l1h->config.bsic_sent = false;
 	l1h->config.bsic_acked = false;
 
-	l1h->config.rxgain_valid = false;
-	l1h->config.rxgain = 0;
 	l1h->config.rxgain_sent = false;
 
 	l1h->config.nomtxpower_sent = false;
 	l1h->config.nomtxpower_acked = false;
 
-	l1h->config.maxdly_valid = false;
 	l1h->config.maxdly_sent = false;
 
-	l1h->config.maxdlynb_valid = false;
 	l1h->config.maxdlynb_sent = false;
 
 	for (tn = 0; tn < TRX_NR_TS; tn++) {