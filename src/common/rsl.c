osmocom/osmo-bts

commit c97a7f51e1b15d40e39df4b7d07b3c6534540186

rsl: NACK Chan Activation for lchans on disabled TS
A broken BSC could send a Chan Activation on a TS which has not yet been
enabled (or even configured). This is the case with our TTCN3 tests,
where OML side is currently handled in parallel by an osmo-bsc while
TTCN3 takes care of the RSL side. This can actually be seen as a
malfunctioning BSC, but it was spotted that given this sequence of
events osmo-bts can crash (see ticket below).

Hence, let's NACK any attempt from a BSC to activate an lchan on a
disabled TS.

Related: OS#5249
Change-Id: I9c3b68487c12efc412a057728a561e061560c544

src/common/rsl.c
@@ -1633,6 +1633,13 @@ static int rsl_rx_chan_activ(struct msgb *msg)
 		return rsl_tx_chan_act_nack(lchan, RSL_ERR_EQUIPMENT_FAIL);
 	}
 
+	if (ts->mo.nm_state.operational != NM_OPSTATE_ENABLED ||
+	    ts->mo.nm_state.availability != NM_AVSTATE_OK) {
+		LOGP(DRSL, LOGL_ERROR, "%s rx chan activ but TS not in nm_state oper=ENABLED avail=OK, nack!\n",
+		     gsm_ts_and_pchan_name(ts));
+		return rsl_tx_chan_act_nack(lchan, RSL_ERR_RR_UNAVAIL);
+	}
+
 	if (ts->pchan == GSM_PCHAN_OSMO_DYN) {
 		ts->dyn.pchan_want = dyn_pchan_from_chan_nr(dch->chan_nr);
 		DEBUGP(DRSL, "%s rx chan activ\n", gsm_ts_and_pchan_name(ts));

commit 3e2b7faed6d6e08f5be30e53c1f860482fc0bf89

rsl: Fix all shadow TS being Chan Act NACKed
The OML NM Channel FSM state only apply to primary timeslots, hence we
need to make sure we pick the primary TS (the non-shadow one).

Due to this bug, all channels on shadow TS where NACKed because the
related state was never "Enabled Ok".

Fixes: c97a7f51e1b15d40e39df4b7d07b3c6534540186
Related: OS#5249
Related: OS#5251
Change-Id: If47e4bdd45a05ed1b5709b6e3d541f2830723e37

src/common/rsl.c
@@ -1624,6 +1624,7 @@ static int rsl_rx_chan_activ(struct msgb *msg)
 	struct abis_rsl_dchan_hdr *dch = msgb_l2(msg);
 	struct gsm_lchan *lchan = msg->lchan;
 	struct gsm_bts_trx_ts *ts = lchan->ts;
+	struct gsm_bts_trx_ts *primary_ts;
 	struct tlv_parsed tp;
 	const struct tlv_p_entry *ie;
 	uint8_t type, cause;
@@ -1635,8 +1636,10 @@ static int rsl_rx_chan_activ(struct msgb *msg)
 		return rsl_tx_chan_act_nack(lchan, RSL_ERR_EQUIPMENT_FAIL);
 	}
 
-	if (ts->mo.nm_state.operational != NM_OPSTATE_ENABLED ||
-	    ts->mo.nm_state.availability != NM_AVSTATE_OK) {
+	/* We need to pick the real TS here to check NM state: */
+	primary_ts = ts->vamos.is_shadow ? ts->vamos.peer : ts;
+	if (primary_ts->mo.nm_state.operational != NM_OPSTATE_ENABLED ||
+	    primary_ts->mo.nm_state.availability != NM_AVSTATE_OK) {
 		LOGP(DRSL, LOGL_ERROR, "%s rx chan activ but TS not in nm_state oper=ENABLED avail=OK, nack!\n",
 		     gsm_ts_and_pchan_name(ts));
 		return rsl_tx_chan_act_nack(lchan, RSL_ERR_RR_UNAVAIL);