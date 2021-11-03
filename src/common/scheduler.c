osmocom/osmo-bts

commit fb905b8d235ff2efe6c1cd9fde2b488b311c1cd7

scheduler: Fix check against empty PDCH blocks
msgb_l2len() will return nonzero values if msg->l2h is NULL.
Let's check against msg->l2h being NULL instead.

Related: SYS#5676
Related: SYS#4919
Fixes: 300e31ed135c674cd44526b7503d4664a45a9ec3
Change-Id: I18b61fcaa858a53887191a18d560c2e929478c64

src/common/scheduler.c
@@ -869,7 +869,7 @@ int trx_sched_ph_data_req(struct gsm_bts_trx *trx, struct osmo_phsap_prim *l1sap
 	OSMO_ASSERT(l1sap->oph.msg);
 
 	/* ignore empty frame */
-	if (!msgb_l2len(l1sap->oph.msg)) {
+	if (!l1sap->oph.msg->l2h) {
 		msgb_free(l1sap->oph.msg);
 		return 0;
 	}

commit 5826136a9de53a6c3c78f2a9072ff6f6951c1930

scheduler: Fix FACCH msg with l2len==0 going to lower layers and logging errors
Recent commit (see below) changed a check to avoid weird results for msg
which had l2h=NULL, since l2len would return unexpected results there.
However, some code branches like FACCH or TCH seem to be always setting
l2h even if len=0. Hence, we must test either of the 2 scenarios (null
pointer and l2len()).

This commit fixes the following message appearing all the time during
calls:
"""
TCH/F: Prim has odd len=0 != 23
"""

Fixes: fb905b8d235ff2efe6c1cd9fde2b488b311c1cd7
Related: SYS#5676
Related: SYS#4919
Change-Id: I43152bc8484a35cd004d3303d3a6e6efcdefa890

src/common/scheduler.c
@@ -869,7 +869,7 @@ int trx_sched_ph_data_req(struct gsm_bts_trx *trx, struct osmo_phsap_prim *l1sap
 	OSMO_ASSERT(l1sap->oph.msg);
 
 	/* ignore empty frame */
-	if (!l1sap->oph.msg->l2h) {
+	if (!l1sap->oph.msg->l2h || msgb_l2len(l1sap->oph.msg) == 0) {
 		msgb_free(l1sap->oph.msg);
 		return 0;
 	}