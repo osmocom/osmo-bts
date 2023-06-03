osmocom/osmo-bts

commit a0770250bc70024fcba4ec787587006ff508a6dd

osmo-bts-trx: alloc/free burst buffers in trx_sched_set_lchan()
It's a lot cleaner if the Rx/Tx burst buffers are allocated and free()d
in one place rather than in each lchan handler individually.  Allocate
the buffer(s) during the lchan activation, free() on deactivation.

Regardless of lchan type, allocate the maximim size of 4 * (3 * 2 * 58)
bytes, which is sufficient to store 4 8PSK modulated bursts.  This way
we reduce the load on the memory allocator at the cost of memory
consumption, what should not be a big problem.

Change-Id: I6a5f76023fc492786076a63016f81285b3576c33
Depends: libosmocore.git I1c38ccc2b64ba9046bb3b1221d99cc55ec493802
Related: OS#1572

src/osmo-bts-trx/sched_lchan_pdtch.c
@@ -60,13 +60,6 @@ int rx_pdtch_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 	if (bi->flags & TRX_BI_F_ACCESS_BURST)
 		return rx_rach_fn(l1ts, bi);
 
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, GSM0503_EGPRS_BURSTS_NBITS);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
-
 	/* clear burst */
 	if (bi->bid == 0) {
 		memset(*bursts_p, 0, GSM0503_EGPRS_BURSTS_NBITS);
@@ -156,11 +149,8 @@ int tx_pdtch_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	int rc = 0;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0) {
-		if (!*bursts_p)
-			return -ENODEV;
+	if (br->bid > 0)
 		goto send_burst;
-	}
 
 	/* get mac block from queue */
 	msg = _sched_dequeue_prim(l1ts, br);
@@ -170,17 +160,10 @@ int tx_pdtch_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 		 * take care of filling C0 with dummy bursts to keep expected
 		 * power transmit levels
 		 */
-		goto no_msg;
+		return -ENODEV;
 	}
 
 	/* BURST BYPASS */
-
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, GSM0503_EGPRS_BURSTS_NBITS);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
 
 	/* encode bursts */
 	rc = gsm0503_pdtch_egprs_encode(*bursts_p, msg->l2h, msg->tail - msg->l2h);
@@ -193,7 +176,7 @@ int tx_pdtch_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 			"(len=%ld)\n", (long)(msg->tail - msg->l2h));
 		/* free message */
 		msgb_free(msg);
-		goto no_msg;
+		return -EINVAL;
 	} else if (rc == GSM0503_EGPRS_BURSTS_NBITS) {
 		*mod = TRX_MOD_T_8PSK;
 	} else {
@@ -226,12 +209,4 @@ send_burst:
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, br, "Transmitting burst=%u.\n", br->bid);
 
 	return 0;
-
-no_msg:
-	/* free burst memory */
-	if (*bursts_p) {
-		talloc_free(*bursts_p);
-		*bursts_p = NULL;
-	}
-	return -ENODEV;
 }

src/osmo-bts-trx/sched_lchan_tchf.c
@@ -99,13 +99,6 @@ int rx_tchf_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, bi, "Received TCH/F, bid=%u\n", bi->bid);
 
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 928);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
-
 	/* shift the buffer by 4 bursts leftwards */
 	if (bi->bid == 0) {
 		memcpy(*bursts_p, *bursts_p + 464, 464);
@@ -460,24 +453,14 @@ int tx_tchf_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	struct msgb *msg;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0) {
-		if (!*bursts_p)
-			return -ENODEV;
+	if (br->bid > 0)
 		goto send_burst;
-	}
 
 	/* BURST BYPASS */
 
-	/* allocate burst memory, if not already,
-	 * otherwise shift buffer by 4 bursts for interleaving */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 928);
-		if (!*bursts_p)
-			return -ENOMEM;
-	} else {
-		memcpy(*bursts_p, *bursts_p + 464, 464);
-		memset(*bursts_p + 464, 0, 464);
-	}
+	 /* shift buffer by 4 bursts for interleaving */
+	memcpy(*bursts_p, *bursts_p + 464, 464);
+	memset(*bursts_p + 464, 0, 464);
 
 	/* dequeue a message to be transmitted */
 	msg = tch_dl_dequeue(l1ts, br);

src/osmo-bts-trx/sched_lchan_tchh.c
@@ -122,13 +122,6 @@ int rx_tchh_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, bi, "Received TCH/H, bid=%u\n", bi->bid);
 
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 696);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
-
 	/* shift the buffer by 2 bursts leftwards */
 	if (bi->bid == 0) {
 		memcpy(*bursts_p, *bursts_p + 232, 232);
@@ -361,28 +354,18 @@ int tx_tchh_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	struct msgb *msg;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0) {
-		if (!*bursts_p)
-			return -ENODEV;
+	if (br->bid > 0)
 		goto send_burst;
-	}
 
 	/* BURST BYPASS */
 
-	/* allocate burst memory, if not already,
-	 * otherwise shift buffer by 2 bursts for interleaving */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 696);
-		if (!*bursts_p)
-			return -ENOMEM;
+	 /* shift buffer by 2 bursts for interleaving */
+	memcpy(*bursts_p, *bursts_p + 232, 232);
+	if (chan_state->dl_ongoing_facch) {
+		memcpy(*bursts_p + 232, *bursts_p + 464, 232);
+		memset(*bursts_p + 464, 0, 232);
 	} else {
-		memcpy(*bursts_p, *bursts_p + 232, 232);
-		if (chan_state->dl_ongoing_facch) {
-			memcpy(*bursts_p + 232, *bursts_p + 464, 232);
-			memset(*bursts_p + 464, 0, 232);
-		} else {
-			memset(*bursts_p + 232, 0, 232);
-		}
+		memset(*bursts_p + 232, 0, 232);
 	}
 
 	/* dequeue a message to be transmitted */

src/osmo-bts-trx/sched_lchan_xcch.c
@@ -69,21 +69,6 @@ int rx_data_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, bi, "Received Data, bid=%u\n", bi->bid);
 
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 464);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
-
-	/* UL-SACCH requires additional memory to keep a copy of each previous
-	 * burst set. */
-	if (L1SAP_IS_LINK_SACCH(trx_chan_desc[bi->chan].link_id) && !chan_state->ul_bursts_prev) {
-		chan_state->ul_bursts_prev = talloc_zero_size(l1ts, 464);
-		if (!chan_state->ul_bursts_prev)
-			return -ENOMEM;
-	}
-
 	/* clear burst & store frame number of first burst */
 	if (bi->bid == 0) {
 		memset(*bursts_p, 0, 464);
@@ -174,45 +159,26 @@ int tx_data_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	ubit_t *burst, **bursts_p = &l1ts->chan_state[br->chan].dl_bursts;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0) {
-		if (!*bursts_p)
-			return -ENODEV;
+	if (br->bid > 0)
 		goto send_burst;
-	}
 
 	/* get mac block from queue */
 	msg = _sched_dequeue_prim(l1ts, br);
-	if (msg)
-		goto got_msg;
-
-	LOGL1SB(DL1P, LOGL_INFO, l1ts, br, "No prim for transmit.\n");
-
-no_msg:
-	/* free burst memory */
-	if (*bursts_p) {
-		talloc_free(*bursts_p);
-		*bursts_p = NULL;
+	if (msg == NULL) {
+		LOGL1SB(DL1P, LOGL_INFO, l1ts, br, "No prim for transmit.\n");
+		return -ENODEV;
 	}
-	return -ENODEV;
 
-got_msg:
 	/* check validity of message */
 	if (msgb_l2len(msg) != GSM_MACBLOCK_LEN) {
 		LOGL1SB(DL1P, LOGL_FATAL, l1ts, br, "Prim has odd len=%u != %u\n",
 			msgb_l2len(msg), GSM_MACBLOCK_LEN);
 		/* free message */
 		msgb_free(msg);
-		goto no_msg;
+		return -EINVAL;
 	}
 
 	/* BURST BYPASS */
-
-	/* allocate burst memory, if not already */
-	if (!*bursts_p) {
-		*bursts_p = talloc_zero_size(l1ts, 464);
-		if (!*bursts_p)
-			return -ENOMEM;
-	}
 
 	/* encode bursts */
 	gsm0503_xcch_encode(*bursts_p, msg->l2h);

src/common/scheduler.c
@@ -35,6 +35,7 @@
 #include <osmocom/core/stats.h>
 
 #include <osmocom/gsm/protocol/gsm_08_58.h>
+#include <osmocom/gsm/gsm0502.h>
 #include <osmocom/gsm/a5.h>
 
 #include <osmo-bts/gsm_data.h>
@@ -542,9 +543,7 @@ const struct trx_chan_desc trx_chan_desc[_TRX_CHAN_MAX] = {
 		.chan_nr = RSL_CHAN_OSMO_PDCH,
 
 		/* Rx and Tx, multiple coding schemes: CS-2..4 and MCS-1..9 (3GPP TS
-		 * 05.03, chapter 5), regular interleaving as specified for xCCH.
-		 * NOTE: the burst buffer is three times bigger because the
-		 * payload of EDGE bursts is three times longer. */
+		 * 05.03, chapter 5), regular interleaving as specified for xCCH. */
 		.rts_fn = rts_data_fn,
 		.dl_fn = tx_pdtch_fn,
 		.ul_fn = rx_pdtch_fn,
@@ -1070,26 +1069,24 @@ static void _trx_sched_set_lchan(struct gsm_lchan *lchan,
 		  (active) ? "Activating" : "Deactivating",
 		  trx_chan_desc[chan].name);
 
-	/* free burst memory, to cleanly start with burst 0 */
-	if (chan_state->dl_bursts) {
-		talloc_free(chan_state->dl_bursts);
-		chan_state->dl_bursts = NULL;
-	}
-	if (chan_state->ul_bursts) {
-		talloc_free(chan_state->ul_bursts);
-		chan_state->ul_bursts = NULL;
-	}
-	if (chan_state->ul_bursts_prev) {
-		talloc_free(chan_state->ul_bursts_prev);
-		chan_state->ul_bursts_prev = NULL;
-	}
-
 	if (active) {
 		/* Clean up everything */
 		memset(chan_state, 0, sizeof(*chan_state));
 
 		/* Bind to generic 'struct gsm_lchan' */
 		chan_state->lchan = lchan;
+
+		/* Allocate memory for Rx/Tx burst buffers.  Use the maximim size
+		 * of 4 * (3 * 2 * 58) bytes, which is sufficient to store 4 8PSK
+		 * modulated bursts. */
+		const size_t buf_size = 4 * GSM_NBITS_NB_8PSK_PAYLOAD;
+		if (trx_chan_desc[chan].dl_fn != NULL)
+			chan_state->dl_bursts = talloc_zero_size(l1ts, buf_size);
+		if (trx_chan_desc[chan].ul_fn != NULL) {
+			chan_state->ul_bursts = talloc_zero_size(l1ts, buf_size);
+			if (L1SAP_IS_LINK_SACCH(trx_chan_desc[chan].link_id))
+				chan_state->ul_bursts_prev = talloc_zero_size(l1ts, buf_size);
+		}
 	} else {
 		chan_state->ho_rach_detect = 0;
 
@@ -1097,6 +1094,11 @@ static void _trx_sched_set_lchan(struct gsm_lchan *lchan,
 		trx_sched_queue_filter(&l1ts->dl_prims,
 				       trx_chan_desc[chan].chan_nr,
 				       trx_chan_desc[chan].link_id);
+
+		/* Release memory used by Rx/Tx burst buffers */
+		TALLOC_FREE(chan_state->dl_bursts);
+		TALLOC_FREE(chan_state->ul_bursts);
+		TALLOC_FREE(chan_state->ul_bursts_prev);
 	}
 
 	chan_state->active = active;

commit 282c87ab2de57bd8e36417d8553d39497b3e674c

osmo-bts-trx: fix recent regression in Tx lchan handlers
In my recent patch a0770250, among with the new burst buffer
allocation/release strategy, I introduced a regression:

        /* send burst, if we already got a frame */
-       if (br->bid > 0) {
-               if (!*bursts_p)
-                       return -ENODEV;
+       if (br->bid > 0)
                goto send_burst;
-       }

We used to allocate the burst buffers in Rx/Tx lchan handlers, and
release them in case of an error, e.g. when no block is available
for transmission.  In the case of Tx burst buffers, the state of
Tx burst buffer was additionally used to check if we have a valid
Tx block for transmission, as can be seen in the code snippet above.

As a side effect of my patch, osmo-bts-trx now keeps transmitting
3 out of 4 bursts (br->bid > 0) of the last valid block, until the
next valid Tx block is available for transmission.

This problem was not affecting the CS domain, where it's expected to
have a more or less constant pressure of Tx blocks.  However it did
show up in the PS domain, where in the absence of active TBFs the
PCU may omit DL blocks.

Add a new field 'dl_mask' to struct l1sched_chan_state, similar to
the existing 'ul_mask', and use it to reconstruct the removed logic.

Change-Id: I4538a8fe6b29f8d6eca33ad27d4a9852e3a3e86c
Fixes: a0770250 "osmo-bts-trx: alloc/free burst buffers in trx_sched_set_lchan()"

include/osmo-bts/scheduler.h
@@ -90,6 +90,7 @@ struct l1sched_chan_state {
 	bool			active;		/* Channel is active */
 	ubit_t			*dl_bursts;	/* burst buffer for TX */
 	enum trx_mod_type	dl_mod_type;	/* Downlink modulation type */
+	uint8_t			dl_mask;	/* mask of transmitted bursts */
 	sbit_t			*ul_bursts;	/* burst buffer for RX */
 	sbit_t			*ul_bursts_prev;/* previous burst buffer for RX (repeated SACCH) */
 	uint32_t		ul_first_fn;	/* fn of first burst */

src/osmo-bts-trx/sched_lchan_pdtch.c
@@ -144,13 +144,20 @@ int rx_pdtch_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 int tx_pdtch_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 {
 	struct msgb *msg = NULL; /* make GCC happy */
-	ubit_t *burst, *bursts_p = l1ts->chan_state[br->chan].dl_bursts;
-	enum trx_mod_type *mod = &l1ts->chan_state[br->chan].dl_mod_type;
+	struct l1sched_chan_state *chan_state = &l1ts->chan_state[br->chan];
+	ubit_t *burst, *bursts_p = chan_state->dl_bursts;
+	enum trx_mod_type *mod = &chan_state->dl_mod_type;
+	uint8_t *mask = &chan_state->dl_mask;
 	int rc = 0;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0)
+	if (br->bid > 0) {
+		if ((*mask & 0x01) != 0x01)
+			return -ENOMSG;
 		goto send_burst;
+	}
+
+	*mask = *mask << 4;
 
 	/* get mac block from queue */
 	msg = _sched_dequeue_prim(l1ts, br);
@@ -205,6 +212,8 @@ send_burst:
 
 		br->burst_len = GSM_BURST_LEN;
 	}
+
+	*mask |= (1 << br->bid);
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, br, "Transmitting burst=%u.\n", br->bid);
 

src/osmo-bts-trx/sched_lchan_tchf.c
@@ -450,11 +450,17 @@ int tx_tchf_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	struct l1sched_chan_state *chan_state = &l1ts->chan_state[br->chan];
 	uint8_t tch_mode = chan_state->tch_mode;
 	ubit_t *burst, *bursts_p = chan_state->dl_bursts;
+	uint8_t *mask = &chan_state->dl_mask;
 	struct msgb *msg;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0)
+	if (br->bid > 0) {
+		if ((*mask & 0x01) != 0x01)
+			return -ENOMSG;
 		goto send_burst;
+	}
+
+	*mask = *mask << 4;
 
 	/* BURST BYPASS */
 
@@ -532,6 +538,8 @@ send_burst:
 		chan_state->dl_facch_bursts--;
 		br->flags |= TRX_BR_F_FACCH;
 	}
+
+	*mask |= (1 << br->bid);
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, br, "Transmitting burst=%u.\n", br->bid);
 

src/osmo-bts-trx/sched_lchan_tchh.c
@@ -351,11 +351,17 @@ int tx_tchh_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 	struct l1sched_chan_state *chan_state = &l1ts->chan_state[br->chan];
 	uint8_t tch_mode = chan_state->tch_mode;
 	ubit_t *burst, *bursts_p = chan_state->dl_bursts;
+	uint8_t *mask = &chan_state->dl_mask;
 	struct msgb *msg;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0)
+	if (br->bid > 0) {
+		if ((*mask & 0x01) != 0x01)
+			return -ENOMSG;
 		goto send_burst;
+	}
+
+	*mask = *mask << 2;
 
 	/* BURST BYPASS */
 
@@ -452,6 +458,8 @@ send_burst:
 		chan_state->dl_facch_bursts--;
 		br->flags |= TRX_BR_F_FACCH;
 	}
+
+	*mask |= (1 << br->bid);
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, br, "Transmitting burst=%u.\n", br->bid);
 

src/osmo-bts-trx/sched_lchan_xcch.c
@@ -156,11 +156,18 @@ int rx_data_fn(struct l1sched_ts *l1ts, const struct trx_ul_burst_ind *bi)
 int tx_data_fn(struct l1sched_ts *l1ts, struct trx_dl_burst_req *br)
 {
 	struct msgb *msg = NULL; /* make GCC happy */
-	ubit_t *burst, *bursts_p = l1ts->chan_state[br->chan].dl_bursts;
+	struct l1sched_chan_state *chan_state = &l1ts->chan_state[br->chan];
+	ubit_t *burst, *bursts_p = chan_state->dl_bursts;
+	uint8_t *mask = &chan_state->dl_mask;
 
 	/* send burst, if we already got a frame */
-	if (br->bid > 0)
+	if (br->bid > 0) {
+		if ((*mask & 0x01) != 0x01)
+			return -ENOMSG;
 		goto send_burst;
+	}
+
+	*mask = *mask << 4;
 
 	/* get mac block from queue */
 	msg = _sched_dequeue_prim(l1ts, br);
@@ -194,6 +201,8 @@ send_burst:
 	memcpy(br->burst + 87, burst + 58, 58);
 
 	br->burst_len = GSM_BURST_LEN;
+
+	*mask |= (1 << br->bid);
 
 	LOGL1SB(DL1P, LOGL_DEBUG, l1ts, br, "Transmitting burst=%u.\n", br->bid);
 