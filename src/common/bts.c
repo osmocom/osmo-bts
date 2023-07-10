osmocom/osmo-bts

commit 44c94fdeae11d922d07a8ad2d22b157412f3ae3b

validate RSL "channel rate and type" against VGCS/VBS flags
We already have specfied (but not yet implemented BTS_FEAT_{VBS,VGCS}).

Let's add code to validate "channel rate and type" compatibility
in bts_supports_cm() once a BTS model would actually exhibit those
feature flags.

Change-Id: I5e7f65b44cef6e2f7ad4f152f80a1686a4f511e3
Related: OS#4851

src/common/bts.c
@@ -791,6 +791,16 @@ int bts_supports_cm(const struct gsm_bts *bts,
 		return 1;
 	case RSL_CMOD_SPD_SPEECH:
 		break;
+	case RSL_CMOD_CRT_TCH_GROUP_Bm:
+	case RSL_CMOD_CRT_TCH_GROUP_Lm:
+		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VGCS))
+			return 0;
+		break;
+	case RSL_CMOD_CRT_TCH_BCAST_Bm:
+	case RSL_CMOD_CRT_TCH_BCAST_Lm:
+		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VBS))
+			return 0;
+		break;
 	case RSL_CMOD_SPD_DATA:
 	default:
 		return 0;
@@ -804,6 +814,8 @@ int bts_supports_cm(const struct gsm_bts *bts,
 			return 0;
 		/* fall-through */
 	case RSL_CMOD_CRT_TCH_Bm:
+	case RSL_CMOD_CRT_TCH_GROUP_Bm:
+	case RSL_CMOD_CRT_TCH_BCAST_Bm:
 		switch (cm->chan_rate) {
 		case RSL_CMOD_SP_GSM1:
 			feature	= BTS_FEAT_SPEECH_F_V1;
@@ -825,6 +837,8 @@ int bts_supports_cm(const struct gsm_bts *bts,
 			return 0;
 		/* fall-through */
 	case RSL_CMOD_CRT_TCH_Lm:
+	case RSL_CMOD_CRT_TCH_GROUP_Lm:
+	case RSL_CMOD_CRT_TCH_BCAST_Lm:
 		switch (cm->chan_rate) {
 		case RSL_CMOD_SP_GSM1:
 			feature	= BTS_FEAT_SPEECH_H_V1;

commit 2bdb2201f6a375c7254298ac2f38a2c22500701d

fix bts_supports_cm(): properly check feature flags for VGCS/VBS
cm->spd_ind can take only three values defined in enum rsl_cmod_spd:

  0000 0001   RSL_CMOD_SPD_SPEECH
  0000 0010   RSL_CMOD_SPD_DATA
  0000 0011   RSL_CMOD_SPD_SIGN

According to 3GPP TS 48.058, section 9.3.6, all other values are
reserved, so expecting RSL_CMOD_CRT_TCH_{GROUP,BCAST}_{Lm,Bm} there
is wrong.  These values are part of enum rsl_cmod_crt, so the right
field would be not cm->spd_ind, but cm->chan_rt.

Let's check these channel types in a separate stage, before checking
the requested codec.  Group them with VAMOS specific types for the
sake of consistency.

Change-Id: I914c84be04da819df9e60e2f5ecc5bac9b61b2e5
Fixes: 44c94fdea "validate RSL "channel rate and type" against VGCS/VBS flags"
Related: OS#4851

src/common/bts.c
@@ -791,6 +791,13 @@ bool bts_supports_cm(const struct gsm_bts *bts,
 		return true;
 	case RSL_CMOD_SPD_SPEECH:
 		break;
+	case RSL_CMOD_SPD_DATA:
+	default:
+		return false;
+	}
+
+	/* Stage 1: check support for the requested channel type */
+	switch (cm->chan_rt) {
 	case RSL_CMOD_CRT_TCH_GROUP_Bm:
 	case RSL_CMOD_CRT_TCH_GROUP_Lm:
 		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VGCS))
@@ -801,21 +808,19 @@ bool bts_supports_cm(const struct gsm_bts *bts,
 		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VBS))
 			return false;
 		break;
-	case RSL_CMOD_SPD_DATA:
-	default:
-		return false;
-	}
-
-	/* Before the requested pchan/cm combination can be checked, we need to
-	 * convert it to a feature identifier we can check */
-	switch (cm->chan_rt) {
 	case RSL_CMOD_CRT_OSMO_TCH_VAMOS_Bm:
+	case RSL_CMOD_CRT_OSMO_TCH_VAMOS_Lm:
 		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VAMOS))
 			return false;
-		/* fall-through */
-	case RSL_CMOD_CRT_TCH_Bm:
+		break;
+	}
+
+	/* Stage 2: check support for the requested codec */
+	switch (cm->chan_rt) {
+	case RSL_CMOD_CRT_OSMO_TCH_VAMOS_Bm:
 	case RSL_CMOD_CRT_TCH_GROUP_Bm:
 	case RSL_CMOD_CRT_TCH_BCAST_Bm:
+	case RSL_CMOD_CRT_TCH_Bm:
 		switch (cm->chan_rate) {
 		case RSL_CMOD_SP_GSM1:
 			feature	= BTS_FEAT_SPEECH_F_V1;
@@ -833,12 +838,9 @@ bool bts_supports_cm(const struct gsm_bts *bts,
 		break;
 
 	case RSL_CMOD_CRT_OSMO_TCH_VAMOS_Lm:
-		if (!osmo_bts_has_feature(bts->features, BTS_FEAT_VAMOS))
-			return false;
-		/* fall-through */
-	case RSL_CMOD_CRT_TCH_Lm:
 	case RSL_CMOD_CRT_TCH_GROUP_Lm:
 	case RSL_CMOD_CRT_TCH_BCAST_Lm:
+	case RSL_CMOD_CRT_TCH_Lm:
 		switch (cm->chan_rate) {
 		case RSL_CMOD_SP_GSM1:
 			feature	= BTS_FEAT_SPEECH_H_V1;