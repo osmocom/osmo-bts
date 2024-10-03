414e1ca8edb7d2169183d2129775f28140ee6bd8
Mychaela N. Falconia <falcon@freecalypso.org>
2024-10-03 23:19:09 +0000

    CSD RTP: verify alignment of V.110 frames
    Since the beginning of CSD implementation, OsmoBTS has operated
    with an implicit (unstated) policy that V.110 frames must be
    perfectly aligned within received clearmode RTP packets - a requirement
    which is NOT set anywhere in TS 48.103 or any of the other specs
    it references.  This design policy is sensible from the standpoint
    of implementation complexity (both OsmoBTS and osmo_trau2rtp emit
    such perfectly aligned packets; if someone is building a gateway
    between an Osmocom GSM network and ISDN-style external networks,
    that gateway can act as the aligner), but it should be explicit
    rather than implicit.
    
    Check V.110 frame alignment in received clearmode RTP packets,
    and reject packets that fail this alignment check.
    
    Change-Id: Icd704dc7fa02e60074efc8a29ad7e42ebdf63783

--- src/common/csd_v110.c
+++ src/common/csd_v110.c
@@ -20,6 +20,7 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <errno.h>
 
 #include <osmocom/core/bits.h>
@@ -150,6 +151,20 @@ ra1_ra2:
 	return RFC4040_RTP_PLEN;
 }
 
+static bool check_v110_align(const ubit_t *ra_bits)
+{
+	int i;
+	ubit_t bit0 = 0, bit1 = 1;
+
+	/* The weird code structure is for performance optimization,
+	 * to avoid conditionals inside loops. */
+	for (i = 0; i < 8; i++)
+		bit0 |= ra_bits[i];
+	for (i = 1; i < 10; i++)
+		bit1 &= ra_bits[i * 8];
+	return (bit0 == 0) && (bit1 == 1);
+}
+
 int csd_v110_rtp_decode(const struct gsm_lchan *lchan, uint8_t *data,
 			const uint8_t *rtp, size_t rtp_len)
 {
@@ -181,6 +196,11 @@ int csd_v110_rtp_decode(const struct gsm_lchan *lchan, uint8_t *data,
 	for (unsigned int i = 0; i < desc->num_blocks; i++) {
 		struct osmo_v110_decoded_frame df;
 
+		/* We require our RTP input to consist of aligned V.110
+		 * frames.  If we get misaligned input, let's catch it
+		 * explicitly, rather than send garbage downstream. */
+		if (!check_v110_align(&ra_bits[i * 80]))
+			return -EINVAL;
 		/* convert a V.110 80-bit frame to a V.110 36-/60-bit frame */
 		osmo_v110_decode_frame(&df, &ra_bits[i * 80], 80);
 		if (desc->num_bits == 60)
