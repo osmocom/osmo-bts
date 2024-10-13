osmocom/osmo-bts

commit 66eae187b98f846be282855af354d360328e0f3e

rsl: rsl_handle_chan_mod_ie(): set lchan->csd_mode
Change-Id: I6b1b44cffae1484098709efe496efafacbd354a8
Related: ca418643 "csd_v110_rtp_encode(): properly set E1/E2/E3 bits"
Related: OS#1572

src/common/rsl.c
@@ -242,29 +242,47 @@ static int rsl_handle_chan_mod_ie(struct gsm_lchan *lchan,
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_32000):
 		/* 32.0 kbit/s services, 32.0 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_32k0;
+		lchan->csd_mode = LCHAN_CSD_M_T_32000;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_29000):
 		/* 29.0 kbit/s services, 29.0 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_29k0;
+		lchan->csd_mode = LCHAN_CSD_M_T_29000;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_14400):
 		/* 14.4 kbit/s services, 14.5 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_14k5;
+		lchan->csd_mode = LCHAN_CSD_M_T_14400;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_9600):
 		/* 9.6 kbit/s services, 12.0 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_12k0;
+		lchan->csd_mode = LCHAN_CSD_M_T_9600;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_4800):
 		/* 4.8 kbit/s services, 6.0 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_6k0;
+		lchan->csd_mode = LCHAN_CSD_M_T_4800;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_2400):
+		/* 2.4 kbit/s *and less* services, 3.6 kbit/s radio interface rate */
+		lchan->tch_mode = GSM48_CMODE_DATA_3k6;
+		lchan->csd_mode = LCHAN_CSD_M_T_2400;
+		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_1200):
+		/* 2.4 kbit/s *and less* services, 3.6 kbit/s radio interface rate */
+		lchan->tch_mode = GSM48_CMODE_DATA_3k6;
+		lchan->csd_mode = LCHAN_CSD_M_T_1200;
+		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_600):
+		/* 2.4 kbit/s *and less* services, 3.6 kbit/s radio interface rate */
+		lchan->tch_mode = GSM48_CMODE_DATA_3k6;
+		lchan->csd_mode = LCHAN_CSD_M_T_600;
+		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_T_1200_75):
 		/* 2.4 kbit/s *and less* services, 3.6 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_3k6;
+		lchan->csd_mode = LCHAN_CSD_M_T_1200_75;
 		break;
 
 	default:

commit aeb78dcc778099077e6a56e8344be1acda680745

rsl: rsl_handle_chan_mod_ie(): set lchan->csd_mode for NT CSD
In commit 66eae187, I skipped assigning LCHAN_CSD_M_NT (0) to
lchan->csd_mode for non-transparent channel modes.  I assumed that
the entire gsm_lchan struct was zero-initialized during activation
or release procedures, but this assumption was incorrect.

In fact, some lchan fields are initialized during activation, while
others are initialized during release.  Specifically, lchan->csd_mode
is zero-initialized when the process starts, and then updated only
for transparent mode requests.  For non-transparent mode requests,
this field is not updated, meaning it retains its previous value.

This bug caused incorrect E1/E2/E3 bit settings and missing GSMTAP
RLP output for channels that were previously used for transparent
CSD calls.  This patch is fixing it.

Change-Id: I793ab4dc25fa852eade6f7a3b67ae961ceb7a093
Fixes: 66eae187 "rsl: rsl_handle_chan_mod_ie(): set lchan->csd_mode"
Related: OS#1572, OS#6579

src/common/rsl.c
@@ -205,37 +205,48 @@ static int rsl_handle_chan_mod_ie(struct gsm_lchan *lchan,
 	/* If octet 4 indicates non-transparent data */
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NT_14k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_14k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NT_12k0):
 		lchan->tch_mode = GSM48_CMODE_DATA_12k0;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NT_6k0):
 		lchan->tch_mode = GSM48_CMODE_DATA_6k0;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NT_43k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_43k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NT_28k8):
 		/* 28.8 kbit/s services, 29.0 kbit/s radio interface rate */
 		lchan->tch_mode = GSM48_CMODE_DATA_29k0;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_43k5_14k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_43k5_14k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_29k0_14k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_29k0_14k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_43k5_29k0):
 		lchan->tch_mode = GSM48_CMODE_DATA_43k5_29k0;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_14k5_43k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_14k5_43k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_14k5_29k0):
 		lchan->tch_mode = GSM48_CMODE_DATA_14k5_29k0;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 	case RSL_CMODE(RSL_CMOD_SPD_DATA, RSL_CMOD_CSD_NTA_29k0_43k5):
 		lchan->tch_mode = GSM48_CMODE_DATA_29k0_43k5;
+		lchan->csd_mode = LCHAN_CSD_M_NT;
 		break;
 
 	/* If octet 4 indicates transparent data */