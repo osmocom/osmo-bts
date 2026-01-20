32fd0d2605ea303b366f12dc103a5bef1ebf7e4d
Pau Espin Pedrol <pespin@sysmocom.de>
2026-01-20 12:48:54 +0100

    Avoid accessing struct log_target members directly
    Use newly available APIs instead.
    
    Change-Id: Icd48874cf51d6af78305a30faeae48ba518e46b3
    Depends: libosmocore.git Change-Id Ie48e7e635feb91509b9c034394df4fb16cb931a3

--- src/common/logging.c
+++ src/common/logging.c
@@ -136,12 +136,12 @@ static struct log_info_cat bts_log_info_cat[] = {
 
 static int osmo_bts_filter_fn(const struct log_context *ctx, struct log_target *tgt)
 {
-	uint8_t *sapi = ctx->ctx[LOG_CTX_L1_SAPI];
-	uint16_t *sapi_mask = tgt->filter_data[LOG_FLT_L1_SAPI];
+	uint8_t *sapi = log_get_context(ctx, LOG_CTX_L1_SAPI);
+	uint16_t *sapi_mask = log_get_filter_data(tgt, LOG_FLT_L1_SAPI);
 
-	if ((tgt->filter_map & (1 << LOG_FLT_L1_SAPI)) != 0
-	    && sapi_mask && sapi
-	    && (*sapi_mask & (1 << *sapi)) != 0)
+	if (log_get_filter(tgt, LOG_FLT_L1_SAPI) &&
+	    sapi_mask && sapi &&
+	    (*sapi_mask & (1 << *sapi)) != 0)
 		return 1;
 
 	return 0;
